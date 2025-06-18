7982dcba49446cbe0d7926697355f4f2b2cfc959
Jose Iglesias <joseiglesiasarevalo@gmail.com>
2025-06-18 11:47:53 +0200

    Version 1.0 terminada. Agregamos READme.md, documentacioÃón y requirements.txt

---
+++ READme.md
@@ -0,0 +1,45 @@
+# 🎓 Generador de Cuestionario Interactivo
+Aplicación para crear y realizar exámenes interactivos sobre distintas materias como Matemáticas, Ciencias, Lengua, Historia y Geografía.
+Además, permite registrar tus calificaciones y llevar un seguimiento de tu rendimiento.
+
+---
+## 📦 Requisitos
+
+- pandas ~=2.3.0 
+- colorama ~=0.4.6
+---
+
+## 💻 Instalación Local
+
+```bash
+# Ubícate en la carpeta donde guardarás el proyecto
+git clone https://github.com/Arevaliis/Generador-Cueestionario-Interactivo.git
+cd Generador-Cueestionario-Interactivo
+
+Crear un entorno virtual: 
+  - En Windows: venv\Scripts\activate
+  - En macOS/Linux: source venv/bin/activate
+  
+Activar el entorno virtual: Activa el entorno virtual con el siguiente comando:
+  - En Windows: venv\Scripts\activate
+  - En macOS/Linux: source venv/bin/activate
+  
+Instalar las dependencias: pip install -r requirements.txt
+
+```
+---
+
+## 📌 Estado del Proyecto
+
+| **Versión** | **Estado** |
+|------------| ---------- |
+| 1.0        | ✅ Completado |
+| 2.0        | 🚧 En progreso |
+
+---
+
+## 📫 Contacto
+
+- ✉️ [joseiglesiasarevalo@gmail.com](mailto:joseiglesiasarevalo@gmail.com)  
+- 💼 [LinkedIn](https://www.linkedin.com/in/jose-iglesias-ar%C3%A9valo-812860206/)  
+- 🐙 [GitHub](https://github.com/Arevaliis)
--- src/main.py
+++ src/main.py
@@ -1,35 +1,34 @@
 import csv
 import json
 import random
+import re
 import sys
 
 import pandas as pd
 from colorama import Fore, Style
 
-# TODO Usar Excepciones
-# TODO Guardar puntacion
-# TODO Sistema Ranking
-# TODO Tiempo limite preguntas
-# TODO Test
-# TODO Documentación
-# TODO Archivo Requirement.txt
-# TODO READme.md
-# TODO Archivos .gitignore?
-
 def main():
+    """
+    Esta función ejecuta en bucle el programa hasta que el usuario decide salir.
+    """
     esta_funcionando = True
 
     while esta_funcionando:
         try:
             mostrar_menu()
-            elegir_accion()
-            esta_funcionando = seguir()
+            if 0 < elegir_accion() < 4: # Si introduce un número fuera de los proporcionados no entra en seguir() y vuelve al menu.
+                esta_funcionando = seguir()
         except ValueError:
-            print("Debe ingresar un Numero del 1-3")
+            print("Debe ingresar un Numero dentro del rango de opciones mostradas. Volviendo al menu.")
         except IndexError:
-            print("Error. No puedes introducir campos vacíos. Volviendo al menu.")
+            print("No puedes introducir campos vacíos o fuera de rango. Volviendo al menu.")
+        except KeyError:
+            print("Materia elegida no valida. Volviendo al menu.")
 
 def mostrar_menu():
+    """
+    Esta función muestra el menu de opciones habilitadas.
+    """
     print("""
     ====== Menu ======
         1.Empezar
@@ -39,6 +38,12 @@ def mostrar_menu():
     """)
 
 def elegir_accion():
+    """
+    Esta función determina la tarea elegida por el usuario.
+
+    Returns:
+        int: opción elegida por el usuario
+    """
     opc = int(input("Elija una Opción: "))
     match opc:
         case 1: hacer_examen(elegir_materia())
@@ -46,17 +51,39 @@ def elegir_accion():
         case 3:
                 print("Saliendo...")
                 sys.exit()
-        case _: print("Debe ingresar un Numero del 1-3")
+        case _: print("Debe ingresar un Numero dentro del rango de opciones mostradas.")
+    return opc
 
 def seguir():
-    respuesta = input("¿Quiere volver al menu? (S/N)").upper()
-    return respuesta.startswith("S")
+    """
+    Esta función solicita al usuario si desea seguir en el programa o salir.
+
+    Returns:
+        boolean: True si quiere seguir. False si desea salir.
+    """
+    respuesta = input("¿Quiere volver al menu? (S/N)").upper().strip()
+    if respuesta == "S":
+        return True
+    print("\nSaliendo! Hasta otra!")
+    return False
 
 def cargar_preguntas():
+    """
+    Esta función accede al fichero que contiene las preguntas.
+
+    Returns:
+        json: Archivo con todas las preguntas.
+    """
     with open("../data/preguntas.json", "r", encoding="utf-8") as file:
         return json.load(file)
 
 def elegir_materia():
+    """
+    Esta función permite elegir de qué materia quiere hacer el usuario el examen.
+
+    Returns:
+        str: Devuelve la materia seleccionada
+    """
     print("""
     ===== Materias =====
         1. Matemáticas
@@ -66,17 +93,24 @@ def elegir_materia():
         5. Geografía
     ====================
     """)
-    opc = int(input(("Elije una materia: ")))
+
+    opc = int(input("Elige una materia: "))
     materias = list(cargar_preguntas().keys())
-    return materias[opc - 1]
+    return materias[opc - 1] if opc > 0 else None
 
 def hacer_examen(opc):
+    """
+    Esta función ejecuta el examen. Muestra las preguntas, las opciones y si es correcta la respuesta dada por el usuario.
+
+    Args:
+        opc (str): Materia seleccionada.
+    """
     preguntas = cargar_preguntas()[opc]
     TOTAL_PREGUNTAS = 10
-    lista_preguntas = random.sample(range(0, len(preguntas)), TOTAL_PREGUNTAS)
+    indice_lista_preguntas = random.sample(range(0, len(preguntas)), TOTAL_PREGUNTAS)
     numero_aciertos = 0
 
-    for i in lista_preguntas:
+    for i in indice_lista_preguntas:
         pregunta = preguntas[i]
         print(pregunta["pregunta"] + "\n")
 
@@ -90,6 +124,16 @@ def hacer_examen(opc):
     guardar_puntuacion(numero_aciertos)
 
 def comprobar_respuesta(resp, correcta):
+    """
+    Esta función comprueba si la respuesta dada por el usuario es igual que la contenida.
+
+    Args:
+        resp (str): Respuesta introducida por el usuario.
+        correcta (str): Respuesta correcta.
+
+    Returns:
+        int: 1 si son iguales 0 si no.
+    """
     if resp == correcta:
         print(Fore.GREEN + "Correcto" + Style.RESET_ALL + "\n")
         return 1
@@ -98,7 +142,17 @@ def comprobar_respuesta(resp, correcta):
         return 0
 
 def resultado_final(total_preguntas, aciertos):
-    valoracion = "Lo has hecho genial !!" if aciertos >= 7 else ("Bien!" if aciertos >= 5 else "Necesitas practicas mas. Tu puedes !!")
+    """
+    Esta función muestra por pantalla un mensaje de como le fue el examen al usuario.
+
+    Args:
+        total_preguntas (int): Número total de preguntas del examen.
+        aciertos (int): Aciertos totales del usuario.
+
+    Returns:
+        tipo: Descripción de lo que retorna la función.
+    """
+    valoracion = "Lo has hecho genial !!" if aciertos >= 7 else ("Bien!" if aciertos >= 5 else "Necesitas practicar más. Tu puedes !!")
 
     print(f"""
     ===== Resultado Final Test =====
@@ -106,40 +160,66 @@ def resultado_final(total_preguntas, aciertos):
         Total de Preguntas = {total_preguntas}
         Numero de Aciertos = {aciertos}
         Porcentaje de Aciertos = {(aciertos / total_preguntas) * 100}%
-        Valoracion final = {valoracion}
+        Valoración final = {valoracion}
         
     =================================
     """)
 
 def guardar_puntuacion(aciertos):
-    resp = input("¿Quieres guardar tu puntuacion?: (S/N)").upper().strip()
+    """
+    Esta función pregunta al usuario si quiere registrar su marca dentro del historial.
+
+    Args:
+        aciertos (int): Aciertos totales del usuario.
+    """
+    resp = input("¿Quieres guardar tu puntuación?: (S/N)").upper().strip()
 
     match resp:
         case "S": almacenar_datos_partida(aciertos)
-        case "_": print("De acuerdo")
+        case _: print("\nLa proxima vez será!!\n")
 
 def almacenar_datos_partida(aciertos):
-    nombre_usuario = input("Introduce tu nombre para el ranking: ").capitalize()
+    """
+    Esta función almacena el nombre y la puntuación del usuario en el historial.
 
-    with open("../data/ranking.csv", "a") as file:
-        return file.write(f"{nombre_usuario} , {aciertos}")
+    Args:
+        aciertos (int): Aciertos totales del usuario.
+
+    Returns:
+        _: Nada, es solo para que termine la ejecución de la función.
+    """
+    nombre_usuario = input("Introduce tu nombre para el ranking: ").title().strip()
+
+    if re.search(r'[^a-zA-Z]', nombre_usuario):
+        with open("../data/ranking.csv", "a") as file:
+            print("Marca recogida.\n")
+            file.write(f"{nombre_usuario},{aciertos}\n")
+            return
+    print("El nombre solo puede contener letras.\n")
 
 def ordenar_ranking():
+    """
+    Esta función hace uso de la libreria pandas para facilitar la tarea de ordenar la clasificación.
+    """
     df_ranking = pd.read_csv("../data/ranking.csv")
     df_ordenado = df_ranking.sort_values(by=df_ranking.columns[1], ascending=False)
     df_ordenado.to_csv("../data/ranking.csv", index=False)
 
 def ver_clasficacion():
+    """
+    Esta función nos muestra la clasificación.
+    """
     ordenar_ranking()
     with open("../data/ranking.csv", "r") as file:
         ranking = csv.reader(file, delimiter=",")
 
-        print("Nombre -- Calificación")
+        print("\nNombre -- Calificación")
         for i, row in enumerate(ranking, 0):
             if i == 0:
                 continue
             nombre, calificacion = row
-            print(f"\t{i}.{nombre.title()} -> {calificacion}")
+            print(f"\t{i}.{nombre} -> {calificacion}")
+        print()
 
 if __name__ == "__main__":
     main()
